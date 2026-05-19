fb8defa69689e037616f33c0ee4ba50b2a8a1915
Mohit Raj <mohitraj854334@gmail.com>
2026-05-19 08:20:38 +0530

    Custom HashSet Of Students Implementation

--- CollectionFramework/src/SetLinkedHashSet.java
+++ CollectionFramework/src/SetLinkedHashSet.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -19,5 +20,16 @@ public class SetLinkedHashSet {
         set.add(70);
         set.add(80);
         System.out.println("Original Set :"+set);
+
+        //Hashset for Student or for custom class
+        // we need to override equals() and hashcode() so that they treat duplicate objects as same
+        Set<Student> set2 = new HashSet<>();
+        Student S1 = new Student(20, "John");
+        Student S2 = new Student(20, "John");
+        Student S3 = new Student(20, "John");
+        set2.add(S1);
+        set2.add(S2);
+        set2.add(S3);
+        System.out.println("Original Student Set :"+set2);
     }
 }
