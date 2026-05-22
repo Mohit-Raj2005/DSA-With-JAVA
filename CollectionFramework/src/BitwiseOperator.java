05f35657a346dd16f1e6db4ba292a51f386df40d
Mohit Raj <mohitraj854334@gmail.com>
2026-05-22 23:53:44 +0530

    Done other operators

--- CollectionFramework/src/BitwiseOperator.java
+++ CollectionFramework/src/BitwiseOperator.java
@@ -11,5 +11,24 @@ public class BitwiseOperator {
 
         //bitwise XOR ^ operator
         System.out.println(a ^ b );
+
+        //NOT Operator also returns in 2s complement
+        //if the leftmost bit of the number is 1 then the number is -ve and if it is 0 then it is +ve
+        System.out.println(~a );
+
+        // << left shift operator
+        // this moves each bit of the number by 1 to the left
+        //if i left shift a number 4 then it becomes 2*4=8 or can say 2N where N is the number which is being shifted
+        // in extreme case 1 after 32 shifts will reach the lsb and the compiler will treat it as a -ve number
+
+        int n=1;
+        for (int i=1 ; i <=32; i++){
+            n= n<<1;
+            System.out.println(n);
+            System.out.println();
+
+        }
+
+
     }
 }
