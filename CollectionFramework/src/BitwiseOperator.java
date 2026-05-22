4b615289d9541d03ef4d7c297aa108d082feff7a
Mohit Raj <mohitraj854334@gmail.com>
2026-05-22 23:40:05 +0530

    Bitwise Operator

---
+++ CollectionFramework/src/BitwiseOperator.java
@@ -0,0 +1,15 @@
+public class BitwiseOperator {
+    static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        //bitwise AND & operator
+        System.out.println(a & b );
+
+        //bitwise OR | operator
+        System.out.println(a | b );
+
+        //bitwise XOR ^ operator
+        System.out.println(a ^ b );
+    }
+}
